4e284c9f3fe7807c4ae69c83e334070206bc2506
hoangarizona <hoangarizona@gmail.com>
2022-12-11 13:37:39 -0700

    create server and print the request

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
-const server = http.createServer(function(req, res){
+const server = http.createServer((req, res)=>{
   console.log(req);
 });
 
-ser
+server.listen(3000);
