cc76af14351f14359ce28e05763be5ea12f5c52a
hoangarizona <hoangarizona@gmail.com>
2022-12-11 15:34:54 -0700

    setHeader, write, end

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require('http');
 const server = http.createServer((req, res)=>{
-  console.log(req);
-  process.exit();
+  //console.log(req.url, req.method, req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Hello</title></head>')
+  res.write('<body><h1>Hello from my Node.js server!</body>');
+  res.write('</html>');
+  res.end();//no more write after this because it already send res to the client
+
 });
 
 server.listen(3000);
