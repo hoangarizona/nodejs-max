4143b4e26fa1b061b7aac31b9db0266f2c94a3dd
hoangarizona <hoangarizona@gmail.com>
2022-12-12 14:17:41 -0700

    redirecting request

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req, res)=>{
+  const method = req.method;
   const url = req.url;
   if(url === '/'){
     res.write('<html>');
@@ -7,6 +9,12 @@ const server = http.createServer((req, res)=>{
     res.write('<body><form action="/message" method="POST"><input type="text name="message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
     return res.end();//have return so we dont run another setHeader, write, end
+  }
+  if(url === '/message' && method === 'POST'){
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;//redirecting
+    res.setHeader('Location','/');//redirecting
+    return res.end();
   }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
