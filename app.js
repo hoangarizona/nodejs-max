6e2fb30458d6b142211cbc26af31bdd00ad86d94
hoangarizona <hoangarizona@gmail.com>
2022-12-11 21:41:16 -0700

    route requests

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const http = require('http');
 const server = http.createServer((req, res)=>{
-  //console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  if(url === '/'){
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>')
+    res.write('<body><form action="/message" method="POST"><input type="text name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();//have return so we dont run another setHeader, write, end
+  }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
-  res.write('<head><title>Hello</title></head>')
-  res.write('<body><h1>Hello from my Node.js server!</body>');
+  res.write('<head><title>Enter Message</title></head>')
+  res.write('<body><h1>Hello from my Node.js server!</h1></body>');
   res.write('</html>');
-  res.end();//no more write after this because it already send res to the client
-
+  return res.end();//no more write after this because it already send res to the client
 });
 
 server.listen(3000);
